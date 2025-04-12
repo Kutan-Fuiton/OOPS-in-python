86c580d1c054d0ba980ac168b5b54eeaf79dc38c
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2025-04-12 11:16:09 +0530

    Add files via upload

--- oops5.py
+++ oops5.py
@@ -8,3 +8,39 @@ s1 = Student("Karan")
 print(s1.name)
 del s1.name
 print(s1.name)
+
+
+
+
+# Private(like) attributes and methods: we can make the attributes and methods private by using __ before the name of the attribute or method. So that it cannot be accessed outside the class. But it can be accessed inside the class. So we can use this to make the attributes and methods private. But this is not a strict rule, so we can access them outside the class also, but it is not recommended to do so. to make a attribute private just have to add two underscores ("__") before that attribute. like self.acc_pass = acc_pass, instead we will do this self.__acc_pass = acc_pass to make it private.
+# Private attributes and methods are meant to be used only wihtin the class and are not accessible from outside the class.
+
+
+class Account: 
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass
+
+    def reset_pass(self):
+        print(self.__acc_pass)
+
+acc1 = Account ("12345","abcde")
+print(acc1.acc_no)
+# print(acc1.acc_pass)
+print(acc1.reset_pass())
+
+
+class Person:
+    __name = "anonymous" # private attribute
+
+    # private method
+    def __hello(self):
+        print("Hello Person!")
+
+    def welcome(self):
+        self.__hello()
+
+p1 = Person()
+print(p1.welcome())
+
+
---
+++ oops6.py
@@ -0,0 +1,121 @@
+# Inheritance --> When one class(child/derived) derives the properties and methods of another class(parent/base).
+
+# Syntax: 
+# class ParentClass:
+#      ......
+# class ChildClass(ParentClass):
+#      ......
+
+
+
+# There are three types of inheritance:
+
+# 1. Single Inheritance: One class inherits from another class.
+# For Example:-
+
+class Car:
+    color = "red"
+    @staticmethod
+    def start():
+        print("Car started..")
+
+    @staticmethod
+    def stop():
+        print("Car stopped..")
+
+class ToyotaCar(Car):
+    def __init__(self, name):
+        self.name = name
+
+car1 = ToyotaCar("Fortuner")
+car2 = ToyotaCar("prius")
+
+print(car1.start())
+print(car1.color)
+
+
+# 2. Multilevel Inheritance: One class inherits from another class, which in turn inherits from another class.
+# For example:-
+
+class Car:
+    color = "red"
+    @staticmethod
+    def start():
+        print("Car started..")
+
+    @staticmethod
+    def stop():
+        print("Car stopped..")
+
+class ToyotaCar(Car):
+    def __init__(self, brand):
+        self.brand = brand
+
+class Fortuner(ToyotaCar):
+    def __init__(self, type):
+        self.type = type
+
+car1 = Fortuner("diesel")
+car1.start()
+
+
+# 3. Multiple Inheritance: One class inherits from multiple classes.
+# For example:-
+
+class A:
+    varA = "welcome to class A"
+
+class B:
+    varB = "welcome to class B"
+
+class C(A, B):
+    varC = "welcome to class C"
+
+c1 = C()
+print(c1.varC) 
+print(c1.varB)
+print(c1.varA)
+
+
+
+
+# Super() function: The super() function is used to call the parent class methods and properties. super() method is used to access methods of the parent class.
+# For example:-
+
+class Car:
+    def __init__(self, type):
+        self.type = type
+
+    @staticmethod
+    def start():
+        print("Car started..")
+
+    @staticmethod
+    def stop():
+        print("Car stopped..")
+
+class ToyotaCar(Car):
+    def __init__(self, name, type):
+        self.name = name
+        #self.type = type # here if this is not defined then it will show error as type is not defined in the child class. But i need to use the parent class type. So i will use super() method to access the parent class type.
+        super().__init__(type)
+        super().start()
+
+car1 = ToyotaCar("prius","electric")
+print(car1.type)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
